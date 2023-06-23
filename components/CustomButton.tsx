99abc3e7c8558582a16aeb1a4867742ccc35ae16
Kohei Nakaji <kobejj@gmail.com>
2023-06-22 21:27:24 -0400

    initial commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Fascinate } from "next/font/google";
+import { CustomButtonProps } from "@/types";
+import { isContext } from "vm";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ components/ShowMore.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName);
+  };
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
+
+// "use client";
+// import React from "react";
+// import { ShowMoreProps } from "@/types";
+// import { useRouter } from "next/navigation";
+// import CustomButton from "./CustomButton";
+// import { updateSearchParams } from "@/utils";
+
+// const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+//   const router = useRouter();
+
+//   const handleNavigation = () => {
+//     const newLimit = (pageNumber + 1) * 10;
+//     setLimit(newLimit);
+//   };
+//   return (
+//     <div className="w-full flex-center gap-5 mt-10">
+//       {!isNext && (
+//         <CustomButton
+//           title="Show More"
+//           btnType="button"
+//           containerStyles="bg-primary-blue rounded-full text-white"
+//           handleClick={handleNavigation}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ShowMore;
